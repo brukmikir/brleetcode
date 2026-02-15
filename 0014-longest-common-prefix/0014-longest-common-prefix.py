321c2af7bdd0fc56dd6a523efb5ad5f546f97059
Biruk_Mikir <brukmikir@gmail.com>
2026-02-15 08:29:24 -0800

    Time: 0 ms (100%), Space: 12.6 MB (37.72%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        shortest = min(strs, key=len)
+
+        for i, ch in enumerate(shortest):
+            for word in strs:
+                if word[i] != ch:
+                    return shortest[:i]
+
+        return shortest
