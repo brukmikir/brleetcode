d0ab3c083f9e67cf04be7a26482f0bb75c487a50
Biruk_Mikir <brukmikir@gmail.com>
2026-02-19 08:35:10 -0800

    Time: 0 ms (100%), Space: 12.4 MB (58.66%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rotate(self, matrix):
+        for i in range(len(matrix)):
+            for j in range(i,len(matrix[0])):
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j] 
+        
+        for r in matrix:
+            r.reverse()
+        return matrix
+        
+
+        
+        
