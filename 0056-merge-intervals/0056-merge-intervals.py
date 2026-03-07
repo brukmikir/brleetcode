104855530a04b1326136d1d702ae797723de3f62
Biruk_Mikir <brukmikir@gmail.com>
2026-03-07 04:31:29 -0800

    Time: 9 ms (68.97%), Space: 17 MB (28.32%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def merge(self, intervals):
+        intervals.sort()
+        res = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            
+            if start <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], end)
+            else:
+                res.append([start, end])
+
+        return res
+
+
+
+        
+        
