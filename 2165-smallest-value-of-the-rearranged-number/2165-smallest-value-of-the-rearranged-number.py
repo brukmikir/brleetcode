9490af1440436a0bc8874ef820e436a55305bd6a
Biruk_Mikir <brukmikir@gmail.com>
2026-03-08 08:21:01 -0700

    Time: 0 ms (100%), Space: 12.4 MB (33.33%) - LeetHub

---
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def smallestNumber(self, num):
+        if num == 0:
+            return 0
+        
+        neg = num < 0
+        digits = list(str(abs(num)))
+        
+        if neg:
+            digits.sort(reverse=True)
+        else:
+            digits.sort()
+            if digits[0] == '0':
+                for i in range(1, len(digits)):
+                    if digits[i] != '0':
+                        digits[0], digits[i] = digits[i], digits[0]
+                        break
+        
+        res = int("".join(digits))
+        return -res if neg else res
+
+        
+        
