da0f79b8b77bc60a56f648e34bb28df69bf7f1de
Biruk_Mikir <brukmikir@gmail.com>
2026-03-09 09:23:30 -0700

    Time: 279 ms (13.97%), Space: 12.9 MB (98.54%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findRelativeRanks(self, score):
+        s = sorted(score, reverse=True)
+        res = []
+
+        for i in score:
+            idx = s.index(i)
+
+            if idx == 0:
+                res.append("Gold Medal")
+            elif idx == 1:
+                res.append("Silver Medal")
+            elif idx == 2:
+                res.append("Bronze Medal")
+            else:
+                res.append(str(idx + 1))
+
+        return res
