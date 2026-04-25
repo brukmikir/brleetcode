017b81f63b78e22fbab72000f5ca52823aaf3fd1
Biruk_Mikir <brukmikir@gmail.com>
2026-04-25 11:59:27 -0700

    Time: 539 ms (9.35%), Space: 20.2 MB (29.06%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -0,0 +1,15 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.events = []
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        for s, e in self.events:
+            if max(s, startTime) < min(e, endTime):
+                return False
+        self.events.append((startTime, endTime))
+        return True
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
